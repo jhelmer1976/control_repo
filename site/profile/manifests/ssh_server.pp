1b1e688b2f0ad140f9873c19ba0f69d9b02d0088
jhelmer1976 <sirjamiehelmer@gmail.com>
2017-11-29 09:41:12 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCxyiiSIMHp5OP6Jbgg1p/aHb+219wqzqAKaQ//uwPGKkwI8sP4fLji+8S9dFzbtexHrOJ39GWIzUlDAhCNcEz2oxtUXji8pjdBZJISqwgfYzIjovrdbHLneYHLb/l3UzXrk7XRXoR1oY6TXo/JiJniGyc/4ZZW9fUbj8ec/FCb4gTbOIoiITJ/uT4SDUHeXYmJXeNlksk0x+nUuJOxy9jWhbo30N17mMVXL5Y84KBfFLcSp0WTsns5XhGqQVrDGSpBU6aOCaYxeGQzgSiq1wDMLtBxDfTwIADwk2PUULxpTksQK2DVQ/GoEVozCUwwp9yBNZe8rvvejDtBl4oS8DrN',
 	}  
 }
